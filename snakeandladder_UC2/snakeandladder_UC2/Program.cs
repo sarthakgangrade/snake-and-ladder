051a314920f1d66dabfa108dde0cce1faf302bf3
“sarthak <sarthakgangrade0@gmail.com>
2021-10-21 02:49:58 +0530

    add UC2

---
+++ snakeandladder_UC2/snakeandladder_UC2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeandladder_UC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int player1 = 0;
+            Random random = new Random();
+            player1 = random.Next(1, 6);
+            Console.WriteLine(player1);
+        }
+    }
+}
